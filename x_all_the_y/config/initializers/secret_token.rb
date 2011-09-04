b4c724c9256a969a99996f11110b6710906830d4
zumbojo <zumbojo@gmail.com>
2011-09-04 16:02:39 -0500

    Initial commit

---
+++ x_all_the_y/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+XAllTheY::Application.config.secret_token = 'd1f84894665e2cb6bef038595ef331137a55f79f9384a8af0f030b5eb11ff27e6e51e968278aa6672f0b8aa15420add644a09e50535042af35ad05824cfe91c0'
