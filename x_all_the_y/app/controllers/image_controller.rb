7f650362033f8570e55bf0f4cf0c8336dd31f595
zumbojo <zumbojo@gmail.com>
2011-09-05 12:18:49 -0500

    Optional middle words

--- x_all_the_y/app/controllers/image_controller.rb
+++ x_all_the_y/app/controllers/image_controller.rb
@@ -1,8 +1,14 @@
 class ImageController < ApplicationController
   def show
-    @verb = params[:words].split('/').first.gsub('.',' ')
-    @noun = params[:words].split('/').last.gsub('.',' ')
-    render :text => "#{@verb} all the #{@noun}" 
+    @words = params[:words].gsub('.',' ').split('/')
+    @verb = @words.shift
+    @noun = @words.pop
+
+    if @words.length == 0
+      render :text => "#{@verb} all the #{@noun}" 
+    else
+      render :text => "#{@verb} #{@words} #{@noun}" 
+    end
   end
 
   private 
