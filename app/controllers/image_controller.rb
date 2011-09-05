387182f6dc9c7793d32251167470d61a9b799f37
zumbojo <zumbojo@gmail.com>
2011-09-05 18:55:32 -0500

    Returning base image from image#show.

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,14 +1,20 @@
 class ImageController < ApplicationController
-  def show
-    @words = params[:words].gsub('.',' ').split('/')
-    @verb = @words.shift
-    @noun = @words.pop
+# def show
+#   @words = params[:words].gsub('.',' ').split('/')
+#   @verb = @words.shift
+#   @noun = @words.pop
 
-    if @words.length == 0
-      render :text => "#{@verb} all the #{@noun}" 
-    else
-      render :text => "#{@verb} #{@words} #{@noun}" 
-    end
+#   if @words.length == 0
+#     render :text => "#{@verb} all the #{@noun}" 
+#   else
+#     render :text => "#{@verb} #{@words} #{@noun}" 
+#   end
+# end
+  
+  def show
+    response.headers["Content-Type"] = "image/png"
+    image = Magick::Image.read("#{RAILS_ROOT}/public/images/responsible.png").first
+    render :text => image.to_blob
   end
 
   private 
