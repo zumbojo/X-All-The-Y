6578e9ed5bb8341af98476e06c8b5f3386800a93
zumbojo <zumbojo@gmail.com>
2011-09-05 11:52:09 -0500

    sad? method

--- x_all_the_y/app/controllers/image_controller.rb
+++ x_all_the_y/app/controllers/image_controller.rb
@@ -4,12 +4,15 @@ class ImageController < ApplicationController
     @noun = params[:words].split('/').last
     render :text => "#{@verb} all the #{@noun}" 
 
-    # todo: strip out, handle '?' and '!'
     # todo: spaces via dots
   end
 
   private 
     def show_error_message
       # todo
+    end
+
+    def sad?
+      /\?/.match(request.request_uri) != nil
     end
 end
