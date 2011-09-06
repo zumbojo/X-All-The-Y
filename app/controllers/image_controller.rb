e1e8fea5fe1c75f3b71847d89a9e76f7a8798018
zumbojo <zumbojo@gmail.com>
2011-09-05 19:30:00 -0500

    Merging text and image processing.

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -1,36 +1,37 @@
 class ImageController < ApplicationController
-# def show
-#   @words = params[:words].gsub('.',' ').split('/')
-#   @verb = @words.shift
-#   @noun = @words.pop
-
-#   if @words.length == 0
-#     render :text => "#{@verb} all the #{@noun}" 
-#   else
-#     render :text => "#{@verb} #{@words} #{@noun}" 
-#   end
-# end
-  
   def show
-    response.headers["Content-Type"] = "image/png"
+    # words!
+    @words = params[:words].gsub('.',' ').split('/')
+    @verb = @words.shift
+    @noun = @words.pop
+
+    @text = @words.length == 0 ? "#{@verb} all the #{@noun}" :
+      "#{@verb} #{@words} #{@noun}" 
+    
+    # pictures!
     image = Magick::Image.read("#{RAILS_ROOT}/public/images/responsible.png").first
 
-
     # http://rmagick.rubyforge.org/portfolio.html
-    text = Magick::Draw.new
-    text.annotate(image, 0, 0, 0, 60, "My friend!") {
+    overlay = Magick::Draw.new
+    # todo: use sad? and handle all differences in two different
+    #  annotate blocks
+    overlay.annotate(image, 0, 0, 0, 60, @text) {
         self.gravity = Magick::SouthGravity
         self.pointsize = 48
         self.stroke = 'transparent'
         self.fill = '#0000A9'
         self.font_weight = Magick::BoldWeight
         }
+
+    # output!
+    response.headers["Content-Type"] = "image/png"
     render :text => image.to_blob
   end
 
   private 
     def show_error_message
       # todo
+      # image should be prerendered and static
     end
 
     def sad?
