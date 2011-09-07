6cf1ea091af80b779cc1f224ad9661052912ecf7
zumbojo <zumbojo@gmail.com>
2011-09-06 21:18:57 -0500

    Switching to two-line format.  Sad mode temporarily disabled.

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -2,30 +2,38 @@ class ImageController < ApplicationController
   def show
     # words!
     @words = params[:words].gsub('.',' ').split('/')
-    @verb = @words.shift
-    @noun = @words.pop
+    @first_line = @words.shift
+    @second_line = @words.shift
 
-    @text = @words.length == 0 ? "#{@verb} all the #{@noun}" :
-      "#{@verb} #{@words} #{@noun}" 
-    
     # pictures!
     #
     # basic .annotate examples at 
     # http://rmagick.rubyforge.org/portfolio.html
     if sad?
-      image = Magick::Image.read("#{RAILS_ROOT}/public/images/sad.png").first
-      overlay = Magick::Draw.new
-      overlay.annotate(image, 0, 0, 0, 10, @text.downcase << '?') {
-          self.gravity = Magick::NorthGravity
-          self.pointsize = 60
-          self.fill = 'black'
-          self.font_family = "Impact"
-      }
+#     image = Magick::Image.read("#{RAILS_ROOT}/public/images/sad.png").first
+#     overlay = Magick::Draw.new
+#     overlay.annotate(image, 0, 0, 0, 10, @text.downcase << '?') {
+#         self.gravity = Magick::NorthGravity
+#         self.pointsize = 60
+#         self.fill = 'black'
+#         self.font_family = "Impact"
+#     }
     else
       image = Magick::Image.read("#{RAILS_ROOT}/public/images/responsible.png").first
       overlay = Magick::Draw.new
-      overlay.annotate(image, 0, 0, 0, 10, @text.upcase << '!') {
+      overlay.annotate(image, 0, 0, 0, 10, @first_line.upcase << '!') {
           self.gravity = Magick::NorthGravity
+          self.pointsize = 60
+          self.fill = 'white'
+          # lolcat text spec:
+          # http://news.deviantart.com/article/41903/
+          self.font_family = "Impact"
+          self.stroke = 'black'
+          self.stroke_width = 2
+          self.font_weight = Magick::BoldWeight
+      }
+      overlay.annotate(image, 0, 0, 0, 10, @second_line.upcase << '!') {
+          self.gravity = Magick::SouthGravity
           self.pointsize = 60
           self.fill = 'white'
           # lolcat text spec:
