5569ea21cb0accb3430d39502168ce1f5d612603
zumbojo <zumbojo@gmail.com>
2011-09-06 21:24:50 -0500

    Proper handling of only a single line of input.

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -2,8 +2,8 @@ class ImageController < ApplicationController
   def show
     # words!
     @words = params[:words].gsub('.',' ').split('/')
-    @first_line = @words.shift
-    @second_line = @words.shift
+    @first_line = @words[0]
+    @second_line = @words[1]
 
     # pictures!
     #
@@ -18,12 +18,14 @@ class ImageController < ApplicationController
           self.fill = 'black'
           self.font_family = "Impact"
       }
-      overlay.annotate(image, 0, 0, 0, 10, @second_line.downcase << '?') {
-          self.gravity = Magick::SouthGravity
-          self.pointsize = 60
-          self.fill = 'black'
-          self.font_family = "Impact"
-      }
+      if @second_line
+        overlay.annotate(image, 0, 0, 0, 10, @second_line.downcase << '?') {
+            self.gravity = Magick::SouthGravity
+            self.pointsize = 60
+            self.fill = 'black'
+            self.font_family = "Impact"
+        }
+      end
     else
       image = Magick::Image.read("#{RAILS_ROOT}/public/images/responsible.png").first
       overlay = Magick::Draw.new
@@ -38,17 +40,19 @@ class ImageController < ApplicationController
           self.stroke_width = 2
           self.font_weight = Magick::BoldWeight
       }
-      overlay.annotate(image, 0, 0, 0, 10, @second_line.upcase << '!') {
-          self.gravity = Magick::SouthGravity
-          self.pointsize = 60
-          self.fill = 'white'
-          # lolcat text spec:
-          # http://news.deviantart.com/article/41903/
-          self.font_family = "Impact"
-          self.stroke = 'black'
-          self.stroke_width = 2
-          self.font_weight = Magick::BoldWeight
-      }
+      if @second_line
+        overlay.annotate(image, 0, 0, 0, 10, @second_line.upcase << '!') {
+            self.gravity = Magick::SouthGravity
+            self.pointsize = 60
+            self.fill = 'white'
+            # lolcat text spec:
+            # http://news.deviantart.com/article/41903/
+            self.font_family = "Impact"
+            self.stroke = 'black'
+            self.stroke_width = 2
+            self.font_weight = Magick::BoldWeight
+        }
+      end
     end
 
     # output!
