f11fab2a3aa78765738a83e268781a4b7e2414de
zumbojo <zumbojo@gmail.com>
2011-09-07 22:04:52 -0500

    Moved hyperbole rendering to method.

--- app/controllers/image_controller.rb
+++ app/controllers/image_controller.rb
@@ -5,48 +5,46 @@ class ImageController < ApplicationController
     @first_line = @words[0]
     @second_line = @words[1]
 
-    # pictures!
-    #
-    # basic .annotate examples at 
-    # http://rmagick.rubyforge.org/portfolio.html
-    if sad?
-      @first_line << '?' unless @second_line
+    render_hyperbole
+  end
 
-      image = Magick::Image.read("#{RAILS_ROOT}/public/images/sad.png").first
-      overlay = Magick::Draw.new
-      overlay.annotate(image, 0, 0, 0, 10, @first_line.downcase) {
-          self.gravity = Magick::NorthGravity
-          self.pointsize = 60
-          self.fill = 'black'
-          self.font_family = "Impact"
-      }
-      if @second_line
-        overlay.annotate(image, 0, 0, 0, 10, @second_line.downcase << '?') {
-            self.gravity = Magick::SouthGravity
+  private 
+    def show_error_message
+      # todo
+      # image should be prerendered and static
+    end
+
+    def render_hyperbole
+      # pictures!
+      #
+      # basic .annotate examples at 
+      # http://rmagick.rubyforge.org/portfolio.html
+      if sad?
+        @first_line << '?' unless @second_line
+
+        image = Magick::Image.read("#{RAILS_ROOT}/public/images/sad.png").first
+        overlay = Magick::Draw.new
+        overlay.annotate(image, 0, 0, 0, 10, @first_line.downcase) {
+            self.gravity = Magick::NorthGravity
             self.pointsize = 60
             self.fill = 'black'
             self.font_family = "Impact"
         }
-      end
-    else
-      @first_line << '!' unless @second_line
+        if @second_line
+          overlay.annotate(image, 0, 0, 0, 10, @second_line.downcase << '?') {
+              self.gravity = Magick::SouthGravity
+              self.pointsize = 60
+              self.fill = 'black'
+              self.font_family = "Impact"
+          }
+        end
+      else
+        @first_line << '!' unless @second_line
 
-      image = Magick::Image.read("#{RAILS_ROOT}/public/images/responsible.png").first
-      overlay = Magick::Draw.new
-      overlay.annotate(image, 0, 0, 0, 10, @first_line.upcase) {
-          self.gravity = Magick::NorthGravity
-          self.pointsize = 60
-          self.fill = 'white'
-          # lolcat text spec:
-          # http://news.deviantart.com/article/41903/
-          self.font_family = "Impact"
-          self.stroke = 'black'
-          self.stroke_width = 2
-          self.font_weight = Magick::BoldWeight
-      }
-      if @second_line
-        overlay.annotate(image, 0, 0, 0, 10, @second_line.upcase << '!') {
-            self.gravity = Magick::SouthGravity
+        image = Magick::Image.read("#{RAILS_ROOT}/public/images/responsible.png").first
+        overlay = Magick::Draw.new
+        overlay.annotate(image, 0, 0, 0, 10, @first_line.upcase) {
+            self.gravity = Magick::NorthGravity
             self.pointsize = 60
             self.fill = 'white'
             # lolcat text spec:
@@ -56,18 +54,24 @@ class ImageController < ApplicationController
             self.stroke_width = 2
             self.font_weight = Magick::BoldWeight
         }
+        if @second_line
+          overlay.annotate(image, 0, 0, 0, 10, @second_line.upcase << '!') {
+              self.gravity = Magick::SouthGravity
+              self.pointsize = 60
+              self.fill = 'white'
+              # lolcat text spec:
+              # http://news.deviantart.com/article/41903/
+              self.font_family = "Impact"
+              self.stroke = 'black'
+              self.stroke_width = 2
+              self.font_weight = Magick::BoldWeight
+          }
+        end
       end
-    end
 
-    # output!
-    response.headers["Content-Type"] = "image/png"
-    render :text => image.to_blob
-  end
-
-  private 
-    def show_error_message
-      # todo
-      # image should be prerendered and static
+      # output!
+      response.headers["Content-Type"] = "image/png"
+      render :text => image.to_blob
     end
 
     def sad?
